61f352457adce826096d7c5654931411570f2979
Vardan <logensaifon@mail.ru>
2020-09-08 02:05:38 +0500

    creating file app and class app

---
+++ dice_puzzle_game/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+class App extends Component{
+
+    render(){
+        return(
+            <div>
+                dice_puzzle_game
+            </div>
+        )
+    }
+}
+
+export default App
