2bb7b63f365cb50ba54297f819cc10ae1dd7f372
Vardan <logensaifon@mail.ru>
2020-10-01 01:02:21 +0500

    *move.js removed 3 and more monochromatic cells, fixed the error asynchronous request.

--- dice_puzzle_game/move.js
+++ dice_puzzle_game/move.js
@@ -1,38 +1,81 @@
 import React, { Component } from "react";
+import ObjAdd from "./Add";
 
 let CellColorsProps
-let test = 0
-let ArrCells
+// let ArrCells = document.getElementsByClassName("ColumnCell")
 
-const CheckLackOfColor = ()=>{
-    ArrCells = document.getElementsByClassName("ColumnCell")
+const SwapTheContentsOfTwoCells = ()=>{
+    if (ObjAdd.ArrOnClick.length === 2) {
+        document.getElementsByClassName("ColumnCell")[ObjAdd.ArrOnClick[0][2]].children[ObjAdd.ArrOnClick[0][1]].children[0].style.backgroundColor = ObjAdd.ArrOnClick[1][0]
+        document.getElementsByClassName("ColumnCell")[ObjAdd.ArrOnClick[1][2]].children[ObjAdd.ArrOnClick[1][1]].children[0].style.backgroundColor = ObjAdd.ArrOnClick[0][0]
 
-    for (let i = 0; i < ArrCells.length; i++) {
-        for (let j = 0; j < ArrCells[i].children.length; j++) {
+        ObjAdd.ArrOnClick = []
+        CellColorsProps.CheckCellColor()
+    }
+}
 
-            if (ArrCells[i].children[j].children[0].style.backgroundColor === "") {
-                console.log(test++);
-                CellColorsProps.MovementOColorsInThePillar()
+const RemovingMoreThanTwoConsecutiveColors = ()=>{
+
+    let ManageAsyncRequests = async ()=>{  
+        await CellColorsProps
+
+        let ArrCounterColors = ObjAdd.ArrCounterColors
+        
+        for (const key in ArrCounterColors) {
+
+            for (let i = 0; i < ArrCounterColors[key].length; i++) {
+
+                let Arr = ArrCounterColors[key]
+
+                for (let j = 0; j < ArrCounterColors[key].length; j++) {
+
+                    for (let k = 0; k < ArrCounterColors[key].length; k++) {
+                        
+                        if (Arr[i] !== Arr[j] && Arr[j] !== Arr[k] && Arr[i][1] === Arr[j][1] && Arr[j][1] === Arr[k][1]) {
+
+                            if ((Arr[j][0] - Arr[i][0]) === 1 && (Arr[k][0] - Arr[j][0]) === 1) {
+
+                                document.getElementsByClassName("ColumnCell")[Arr[i][0]].children[Arr[i][1]].children[0].style.backgroundColor = ""
+
+                                document.getElementsByClassName("ColumnCell")[Arr[j][0]].children[Arr[j][1]].children[0].style.backgroundColor = ""
+
+                                document.getElementsByClassName("ColumnCell")[Arr[k][0]].children[Arr[k][1]].children[0].style.backgroundColor = ""
+
+                                CellColorsProps.CheckCellColor()
+                            }
+                            
+                        }
+
+                        if (Arr[i] !== Arr[j] && Arr[j] !== Arr[k] && Arr[i][0] === Arr[j][0] && Arr[j][0] === Arr[k][0]) {
+
+                            if ((Arr[j][1] - Arr[i][1]) === 1 && (Arr[k][1] - Arr[j][1]) === 1) {
+
+                                // console.log(Arr[i],Arr[j],Arr[k]);
+
+                                document.getElementsByClassName("ColumnCell")[Arr[i][0]].children[Arr[i][1]].children[0].style.backgroundColor = ""
+
+                                document.getElementsByClassName("ColumnCell")[Arr[j][0]].children[Arr[j][1]].children[0].style.backgroundColor = ""
+
+                                document.getElementsByClassName("ColumnCell")[Arr[k][0]].children[Arr[k][1]].children[0].style.backgroundColor = ""
+
+                                CellColorsProps.CheckCellColor()
+                            }
+                        }
+                    }
+
+                }
+
             }
-            
         }
     }
+    ManageAsyncRequests()
 }
 
 class MoveCell extends Component{
     constructor(props){
         super(props)
         CellColorsProps = this.props.CellColorsProps
-        this.ManageAsyncRequests()
     }
-
-    ManageAsyncRequests = async ()=>{  
-        await CellColorsProps.CreateColors()
-
-        CheckLackOfColor()
-    }
-
-
 
     render(){
         return(
@@ -43,5 +86,6 @@ class MoveCell extends Component{
     }
 }
 
-export {CheckLackOfColor}
+export {RemovingMoreThanTwoConsecutiveColors}
+export {SwapTheContentsOfTwoCells}
 export default MoveCell
